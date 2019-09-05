7224a326a7f72fef530cc1b2f881e9b65cac6eb3
iwillreku3206 <37181282+iwillreku3206@users.noreply.github.com>
2019-09-05 13:50:10 +0800

    Update score.cs

--- Tetris/Assets/scripts/score.cs
+++ Tetris/Assets/scripts/score.cs
@@ -1,6 +1,43 @@
 using System;
 
 
-public class Scores: MonoBehaviour () {
-
+public class Scores: MonoBehaviour {
+  int score;
+  
+  GameObject scoreBox = GameObject.GetComponent<Text>();
+  
+  //Types of scoring
+  
+  void BlockDown (int level) {
+    
+  }
+  
+  
+  void HardDrop (int level, int height) {
+    
+  }
+  
+  void LineClear (int level, int lines, bool ts, bool perfect, bool b2b) {
+    if (perfect) {
+      if (ts) {
+        
+      } else {
+        
+      }
+      
+    } else {
+      if (ts) {
+        
+      } else {
+        
+      }
+    }
+  }
+  
+  void Update() {
+    // Updates score
+    scoreBox.text = Convert.ToString(score);
+  }
+  
+  
 }
